f2430f18a84e092734989997d83843d509cb1ed6
Atanas Atanasov <naso.003@gmail.com>
2021-11-10 23:04:15 +0200

    update gitignore and run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-dotnet run -- 1 personal.txt
-dotnet run -- 2 common.txt
+REM dotnet run -- 1 personal.txt
+REM dotnet run -- 2 common.txt
 REM dotnet run -- 3 revolut.csv
 dotnet run -- 4 Transactions.csv
