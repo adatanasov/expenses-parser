3f677dd528be018e7707b52a0420eb0da57f65e6
Atanas Atanasov <naso.003@gmail.com>
2026-05-03 13:49:15 +0300

    update run bat

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 REM dotnet run -- 1 personal.txt
 REM dotnet run -- 2 common.txt
 dotnet run -- 3 Revolut.csv
-REM dotnet run -- 4 Transactions.csv
-dotnet run -- 4 TransactionsEUR.csv
+dotnet run -- 4 Transactions.csv
 dotnet run -- 5 Paysera.csv
